27f0368eeed5f8ed2caa1b42d4ee1825cdb24c59
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2022-09-30 19:27:49 +0700

    beryllium: Migrate bootanimation legacy to new

--- xdroid_beryllium.mk
+++ xdroid_beryllium.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/xdroid/config/common.mk)
 XDROID_MAINTAINER := ibratabian17
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
+XDROID_BOOT := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := xdroid_beryllium
